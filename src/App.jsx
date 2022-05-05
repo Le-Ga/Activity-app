83859064f5a83f6cf7b46ce06e5767518629d421
Le-Ga <druliha2@yandex.ru>
2022-05-05 12:40:08 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import './App.scss';
+
+function App() {
+   const [activity, setActivity] = useState([]);
+   const [activityList, setActivityList] = useState([]);
+
+   async function fetchMyApi() {
+      const response = await fetch('https://www.boredapi.com/api/activity/ ');
+      const data = await response.json();
+      setActivity(data.activity);
+      let listArray = activityList;
+      listArray.push(data.activity);
+      setActivityList(listArray);
+
+      if (listArray.length === 5) {
+         document.querySelector('.addBtn').style.display = 'none';
+         document.querySelector('.deleteBtn').style.display = 'block';
+      }
+   }
+
+   const deleteActivity = () => {
+      setActivityList([]);
+      document.querySelector('.addBtn').style.display = 'block';
+      document.querySelector('.deleteBtn').style.display = 'none';
+   }
+
+   return (
+      <div className="wrapper">
+
+         <div className='container'>
+            <div className='activity-card'>
+               <div className="header">Choose an activity!</div>
+               <div></div>
+               <button onClick={fetchMyApi} className="addBtn btn">Show</button>
+               <div className='crutch'>{activity}</div>
+               <ul className="activity-list">
+                  {activityList.map((item, index) => (
+                     <li key={index}>{index + 1}. {item}</li>
+                  ))}
+               </ul>
+               <button className='deleteBtn btn' onClick={deleteActivity}>Delete All</button>
+            </div>
+         </div>
+
+      </div>
+   );
+}
+export default App;
---
+++ src/chek.jsx
@@ -0,0 +1,64 @@
+import './App.css';
+import fone from './fone.jpg'
+import { useState } from "react"
+
+function App() {
+
+   const [listTips, setListTips] = useState([])
+   const [arrayTips, setArrayTips] = useState([])
+
+   const fetchTips = async () => {
+      const response = await fetch("https://www.boredapi.com/api/activity/");
+      const data = await response.json();
+
+      if (listTips.length === 0) {
+         document.querySelector('.deletebtn').style.display = 'block'
+      }
+      if (listTips.length > 3) {
+         document.querySelector('.addbtn').style.display = 'none'
+         document.querySelector('h2').style.display = 'block'
+      }
+      if (listTips.length > 4) {
+         return false
+      }
+
+      arrayTips.push(data.activity)
+      setListTips([...arrayTips])
+   }
+
+   const deleteTips = () => {
+      setListTips([])
+      setArrayTips([])
+      document.querySelector('.deletebtn').style.display = 'none'
+      document.querySelector('.addbtn').style.display = 'block'
+      document.querySelector('h2').style.display = 'none'
+   }
+
+   const wordCross = (event) => {
+      const tipsItem = event.target;
+      tipsItem.classList.toggle('crossed')
+   }
+
+
+   return (
+      <div className="App">
+         <div>
+            <img className='fon' src={fone} alt='фон' />
+         </div>
+
+         <div className='container'>
+            <h1>Get an activity</h1>
+            <button className='addbtn' onClick={fetchTips}>Add Activity</button>
+            <h2>Let's start!</h2>
+            <div className='container-tips'>
+               {listTips.map((item, index) => (
+                  <p onClick={wordCross} key={index}>{index + 1}. {item}</p>
+               ))}
+            </div>
+            <button className='deletebtn' onClick={() => deleteTips()}>Delete All</button>
+         </div>
+      </div>
+   );
+}
+
+export default App;
